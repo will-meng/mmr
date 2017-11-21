61901ff288983b6fee472c5db74da797cfca898c
William Meng <willmeng@gmail.com>
2017-11-21 15:37:06 -0800

    Update homepage title and message

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'; 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to RunMyMap</h1>, root);
+  const store = configureStore();
+
+  // Testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // End Testing
+
+  ReactDOM.render(<h1>Welcome to MyMapRun</h1>, root);
 });
