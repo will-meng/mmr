c89b292d20c427eada28feb2e069da34c49a1db5
William Meng <willmeng@gmail.com>
2017-11-22 11:40:09 -0800

    Remove public folder for heroku push

--- frontend/components/routes/run_map.jsx
+++ frontend/components/routes/run_map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { withRouter } from 'react-router-dom';
 
-// import MarkerManager from '../../util/marker_manager';
+import MarkerManager from '../../utils/marker_manager';
 
 // const getCoordsObj = latLng => ({
 //   lat: latLng.lat(),
@@ -21,11 +21,19 @@ class RunMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {waypoints: {}};
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setMap(this.map);
   }
+
   componentDidMount() {
     const map = this.refs.map;
     this.map = new google.maps.Map(map, mapOptions);
-    // this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager = new MarkerManager(
+      this.map, 
+      this.handleMarkerClick.bind(this),
+      this.handleMarkerDrag.bind(this)
+    );
     this.registerListeners();
   }
 
@@ -34,41 +42,57 @@ class RunMap extends React.Component {
       this.handleClick(event.latLng);
     });
   }
+  
+  handleClick(latLng) {
+    const len = this.waypointIds().length;
+    if (len < 23) {
+      const lastIdx = parseInt(this.waypointIds()[len - 1]) + 1 || 1;
+      const waypoint = { id: lastIdx, latLng };
+      this.MarkerManager.createMarker(waypoint);
+      
+      const waypoints = this.state.waypoints;
+      waypoints[lastIdx] = waypoint;
+      this.setState({waypoints: waypoints});
+    }
+  }
+  
+  handleMarkerClick(waypoint) {
+    const waypoints = this.state.waypoints;
+    delete waypoints[waypoint.id];
+    this.setState({waypoints: waypoints});
+  }
+
+  handleMarkerDrag(waypoint) {
+    const waypoints = this.state.waypoints;
+    waypoints[waypoint.id] = waypoint;
+    this.setState({waypoints: waypoints});
+  }
 
   waypointIds() {
     return Object.keys(this.state.waypoints);
   }
-
-  handleClick(latLng) {
-    const len = this.waypointIds().length;
-    if (len < 23) {
-      console.log(this.waypointIds());
-      console.log((this.waypointIds()[len - 1] + 1));
-      console.log((this.waypointIds()[len - 1] + 1) || 1);
-      const lastIdx = (this.waypointIds()[len - 1] + 1) || 1;
-      const newWaypoints = this.state.waypoints;
-      newWaypoints[lastIdx] = latLng;
-      this.setState({waypoints: newWaypoints});
-    }
-  }
-
+  
   render() {
     const waypoints = this.state.waypoints;
     return (
       <div>
+        <div className="map" ref="map">
+          Map
+        </div>
+
+        {/* TESTING output lat/lng */}
         <ul>
           {
           this.waypointIds().map(id => 
               <li key={id}>
-                {`Lat ${id}: ${waypoints[id].lat()} 
-                  Lng ${id}: ${waypoints[id].lng()}`}
+                {`Lat ${id}: ${waypoints[id].latLng.lat()} 
+                  Lng ${id}: ${waypoints[id].latLng.lng()}`}
               </li>
             )
           }
         </ul>
-        <div className="map" ref="map">
-          Map
-        </div>
+        {/* TESTING end */}
+
       </div>
     );
   }
--- frontend/utils/marker_manager.js
+++ frontend/utils/marker_manager.js
@@ -1,8 +1,39 @@
 class MarkerManager {
-  constructor(map){
+  constructor(map, handleClick, handleDrag){
     this.map = map;
+    this.handleClick = handleClick;
+    this.handleDrag = handleDrag;
     this.markers = {};
+    this.labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.labelIndex = 0;
   }
 
-  
+  createMarker(waypoint) {
+    const marker = new google.maps.Marker({
+      position: waypoint.latLng,
+      map: this.map,
+      id: waypoint.id,
+      label: this.labels[this.labelIndex++ % 26],
+      draggable: true,
+    });
+
+    marker.addListener('click', () => {
+      this.removeMarker(marker);
+      return this.handleClick(waypoint);
+    });
+
+    marker.addListener('dragend', () => {
+      waypoint.latLng = marker.position;
+      return this.handleDrag(waypoint);
+    });
+
+    this.markers[marker.id] = marker;
+  }
+
+  removeMarker(marker) {
+    this.markers[marker.id].setMap(null);
+    delete this.markers[marker.id];
+  }
 }
+
+export default MarkerManager;
