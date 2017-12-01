e4049390c4a8bbcc41a66f209b0c888020935c72
William Meng <willmeng@gmail.com>
2017-12-01 15:37:08 -0800

    Modify package.json and store.js to hide logger in prod

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const configureStore = (preloadedState = {}) => (
-   createStore(rootReducer, preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger)))
-);
+const middlewares = [thunk];
+
+const configureStore = (preloadedState = {}) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { logger } = require('redux-logger');
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    middlewares.push(logger);
+    return createStore(rootReducer, preloadedState,
+      composeWithDevTools(applyMiddleware(...middlewares)));
+  }
+  return createStore(rootReducer, preloadedState,
+    applyMiddleware(...middlewares));
+};
 
 export default configureStore;
