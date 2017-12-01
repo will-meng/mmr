3bfb4c55b9b6efd86b98dc1649a04e116274fac9
William Meng <willmeng@gmail.com>
2017-12-01 15:53:41 -0800

    Fix bug with current date

--- frontend/components/workouts/workout_form/workout_form.jsx
+++ frontend/components/workouts/workout_form/workout_form.jsx
@@ -5,8 +5,8 @@ import { formatDate } from '../../../utils/route_api_util';
 export const _todaysDate = () => {
   const date = new Date();
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
   return year + '-' + month + '-' + day;
 };
 
